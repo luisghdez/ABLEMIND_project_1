6a4e814309294d67df59d7887fd79563fd155358
Luis Hernandez <1gluishernandez@gmail.com>
2022-09-22 13:50:01 -0600

    see company page and add a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+  def new
+    @newproduct = Product.new
+    @categories = Category.all.sort_by(&:name)
+  end
+
+  def create
+    mysup = Supplier.find_by(user_id: current_user.id)
+    @newproduct = Product.new(products_params)
+    @newproduct.supplier = mysup
+    @newproduct.category = Category.find(params[:product][:category])
+    if @newproduct.save
+      redirect_to supplier_path(mysup[:id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def products_params
+    params.require(:product).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :suppliers, only: [:index, :show, :new, :create]
+  resources :suppliers, only: %i[index show new create]
+  resources :products, only: %i[new create delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
