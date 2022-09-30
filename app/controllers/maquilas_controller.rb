322a8fe0332075b74c62d553e1617d482eb59a1f
Luis Hernandez <1gluishernandez@gmail.com>
2022-09-30 15:55:04 -0600

    succesfully register either as maquila or user

--- app/controllers/maquilas_controller.rb
+++ app/controllers/maquilas_controller.rb
@@ -1,3 +1,21 @@
 class MaquilasController < ApplicationController
+  def new
+    @newmaquila = Maquila.new
+  end
 
+  def create
+    @newmaquila = Maquila.new(maquilas_params)
+    @newmaquila.user = current_user
+    if @newmaquila.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def maquilas_params
+    params.require(:maquila).permit(:name)
+  end
 end
