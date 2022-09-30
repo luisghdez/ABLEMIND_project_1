2b0820ff2bf88ff9e54f23df7410419c041eb4dd
Luis Hernandez <1gluishernandez@gmail.com>
2022-09-30 15:42:12 -0600

    added registration controller to redirect depending on user choice on registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'favorite_companies/create'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+    # , sessions: 'users/sessions'
+  }
   root to: "pages#home"
 
   resources :favorite_companies, only: %i[index create]
   resources :suppliers, only: %i[index show new create]
+  resources :maquilas, only: %i[new create]
 
   resources :posts, only: %i[new create delete] do
     resources :likes, only: %i[create delete]
