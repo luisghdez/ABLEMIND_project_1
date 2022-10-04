30e8a7262e50f29db81f7d0989c15c2233551593
Luis Hernandez <1gluishernandez@gmail.com>
2022-10-04 12:23:48 -0600

    d

--- app/javascript/controllers/my_navbar_controller.js
+++ app/javascript/controllers/my_navbar_controller.js
@@ -5,13 +5,12 @@ export default class extends Controller {
     console.log("1connected")
   }
 
-  // myFunction() {
-  //   var x = document.getElementById("myTopnav");
-  //   if (x.className === "topnav") {
-  //     x.className += " responsive";
-  //   } else {
-  //     x.className = "topnav";
-  //   }
-  //   console.log('connected');
-  // };
+  myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  };
 }
